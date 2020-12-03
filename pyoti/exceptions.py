a13f3ccf71f5c9fc6356e662d378fbb631957a74
goodlandsecurity <josing.jj@gmail.com>
2020-12-03 17:12:57 -0600

    fixed formatting

--- pyoti/exceptions.py
+++ pyoti/exceptions.py
@@ -18,20 +18,25 @@ class SpamhausZenError(PyOTIError):
     """Exception raised for any special codes returned that indicates an error in query."""
     pass
 
+
 class URLhausHashError(PyOTIError):
     """Exception raised for invalid hash type for URLhaus query."""
     pass
 
+
 class VirusTotalDomainError(PyOTIError):
     """Exception raised for invalid domain for VirusTotal query."""
+
 
 class VirusTotalHashError(PyOTIError):
     """Exception raised for invalid hash type or scan_id for VirusTotal query."""
     pass
 
+
 class VirusTotalIPError(PyOTIError):
     """Exception raised for invalid IP address for VirusTotal query."""
     pass
+
 
 class VirusTotalURLError(PyOTIError):
     """Exception raised for invalid URL for VirusTotal query."""
