68cee80c972095b7661c06af9c1d46a82c1f2ec1
goodlandsecurity <josing.jj@gmail.com>
2021-02-01 17:58:53 -0600

    fix formatting

--- examples/dt_domain_risk_score.py
+++ examples/dt_domain_risk_score.py
@@ -3,6 +3,7 @@ from argparse import ArgumentParser
 from pyoti.domains import IrisInvestigate
 from pyoti.keys import domaintools
 
+
 def run(args):
     iris = IrisInvestigate()
     iris.api_key = domaintools
@@ -14,12 +15,14 @@ def run(args):
     except IndexError:
         return "Iris risk score: N/A"
 
+
 def main():
     parser = ArgumentParser(prog='IrisInvestigate Domain Risk Score', description='Check Domaintools Iris Investigate for domain risk score of a given domain')
     parser.add_argument('-d', '--domain', dest='domain', help='domain to check reputation')
     args = parser.parse_args()
 
     print(run(args.domain))
+
 
 if __name__ == '__main__':
     main()
