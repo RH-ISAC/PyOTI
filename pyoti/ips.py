4c2c4d040be9ffa201811cb86f441ed46de218f8
goodlandsecurity <josing.jj@gmail.com>
2021-02-11 17:19:25 -0600

    reworked both classes

--- pyoti/ips.py
+++ pyoti/ips.py
@@ -1,6 +1,7 @@
 import json
 import requests
 
+from pyoti.exceptions import SpamhausError
 from pyoti.classes import IPAddress
 from pyoti.keys import abuseipdb, spamhausintel
 from pyoti.utils import time_check_since_epoch
@@ -15,25 +16,15 @@ class AbuseIPDB(IPAddress):
 
     :param api_key: AbuseIPDB API key
     :param api_url: AbuseIPDB API URL
-    :param max_age: How far back in time (days) to fetch reports. (defaults to 30 days)
     """
 
-    def __init__(self, api_key=abuseipdb, api_url='https://api.abuseipdb.com/api/v2/check', max_age=30):
-        self._max_age = max_age
+    def __init__(self, api_key=abuseipdb, api_url='https://api.abuseipdb.com/api/v2/check'):
         IPAddress.__init__(self, api_key, api_url)
 
-    @property
-    def max_age(self):
-        return self._max_age
-
-    @max_age.setter
-    def max_age(self, value):
-        self._max_age = value
-
-    def _api_get(self, endpoint):
+    def _api_get(self, max_age):
         params = {
             'ipAddress': self.ip,
-            'maxAgeInDays': self.max_age
+            'maxAgeInDays': max_age
         }
 
         headers = {
@@ -41,19 +32,23 @@ class AbuseIPDB(IPAddress):
             'Key': self.api_key
         }
 
-        response = requests.request("GET", url=endpoint, headers=headers, params=params)
+        response = requests.request("GET", url=self.api_url, headers=headers, params=params)
 
         return response.json()
 
-    def check_ip(self):
-        """Checks IP reputation
+    def check_ip(self, max_age=30):
+        """
+        Checks IP reputation
 
         The check endpoint (api.abuseipdb.com/api/v2/check) accepts a single IP
         address (v4 or v6). Optionally you may set the max_age parameter to only
         return reports within the last X number of days.
+
+        :param max_age: How far back in time (days) to fetch reports. (defaults to 30 days)
+        :return: dict
         """
 
-        response = self._api_get(self.api_url)
+        response = self._api_get(max_age=max_age)
 
         return response
 
@@ -81,9 +76,10 @@ class SpamhausIntel(IPAddress):
         if response.status_code == 200:
             self._token = response.json()["token"]
             self._expires = response.json()["expires"]
-            return response.json()
+        elif response.status_code == 401:
+            raise SpamhausError("Authentication Failed!")
 
-    def _api_get(self, type, ip, mask):
+    def _api_get(self, limit, since, until, type, ip, mask):
         if not self._token:
             self._api_login()
         if not time_check_since_epoch(self._expires):
@@ -91,12 +87,31 @@ class SpamhausIntel(IPAddress):
 
         headers = {'Authorization': f'Bearer {self._token}'}
 
-        response = requests.request("GET", url=f'https://api.spamhaus.org/api/intel/v1/byobject/cidr/XBL/listed/{type}/{ip}/{mask}', headers=headers)
+        params = {
+            'limit': limit,
+            'since': since,
+            'until': until
+        }
+
+        response = requests.request('GET', url=f'https://api.spamhaus.org/api/intel/v1/byobject/cidr/XBL/listed/{type}/{ip}/{mask}', headers=headers, params=params)
 
         return response
 
-    def check_ip(self):
-        get = self._api_get(type="live", ip=self.ip, mask="32")
+    def check_ip(self, limit=None, since=None, until=None, type='live', mask='32'):
+        """
+        :param limit: default None
+        :param since: default 12 months ago (unix timestamp)
+        :param until: default current time (unix timestamp)
+        :param type: default live (history - other option)
+        :param ip: IP Address to check reputation
+        :param mask: default 32
+        :return: dict
+        """
+        get = self._api_get(limit=limit, since=since, until=until, type=type, ip=self.ip, mask=mask)
 
         if get.status_code == 200:
             return get.json()
+        elif get.status_code == 404:
+            return "Not found!"
+        else:
+            raise SpamhausError(get.text)
