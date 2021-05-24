bbd88c7c18cd70e8b8bd99d5ad233c379e5070d0
goodlandsecurity <josing.jj@gmail.com>
2021-05-24 13:16:28 -0500

    added long description

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup
 
+with open("README.md", "r") as f:
+    long_description = f.read()
+
 setup(
     name="pyoti",
     version="0.1",
     description="Python API for Threat Intelligence",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/RH-ISAC/PyOTI",
     author="goodlandsecurity",
     author_email="jj.josing@rhisac.org",
