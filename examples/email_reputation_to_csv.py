5d5498b2f3a6a17ce0ff02fc54f781e823c86f74
goodlandsecurity <josing.jj@gmail.com>
2021-08-10 19:55:27 -0500

    added fields to write to csv

--- examples/email_reputation_to_csv.py
+++ examples/email_reputation_to_csv.py
@@ -17,6 +17,9 @@ def run(args):
         "New Domain",
         "Disposable",
         "Deliverable",
+        "Spoofable",
+        "SPF Strict",
+        "DMARC Enforced"
     ]
 
     with open("email_reputation.csv", "w") as csvfile:
@@ -29,26 +32,35 @@ def run(args):
             eml_rep = eml.check_email()
 
             eml_address = email
-            eml_reputation = eml_rep["reputation"]
-            eml_sus = eml_rep["suspicious"]
-            eml_dmn_exsts = eml_rep["details"]["domain_exists"]
-            eml_dmn_rep = eml_rep["details"]["domain_reputation"]
-            eml_dmn_new = eml_rep["details"]["new_domain"]
-            eml_disposable = eml_rep["details"]["disposable"]
-            eml_deliverable = eml_rep["details"]["deliverable"]
+            try:
+                eml_reputation = eml_rep["reputation"]
+                eml_sus = eml_rep["suspicious"]
+                eml_dmn_exsts = eml_rep["details"]["domain_exists"]
+                eml_dmn_rep = eml_rep["details"]["domain_reputation"]
+                eml_dmn_new = eml_rep["details"]["new_domain"]
+                eml_disposable = eml_rep["details"]["disposable"]
+                eml_deliverable = eml_rep["details"]["deliverable"]
+                eml_spoofable = eml_rep["details"]["spoofable"]
+                eml_spf_strict = eml_rep["details"]["spf_strict"]
+                eml_dmarc_enforced = eml_rep["details"]["dmarc_enforced"]
 
-            csvwriter.writerow(
-                [
-                    eml_address,
-                    eml_reputation,
-                    eml_sus,
-                    eml_dmn_exsts,
-                    eml_dmn_rep,
-                    eml_dmn_new,
-                    eml_disposable,
-                    eml_deliverable,
-                ]
-            )
+                csvwriter.writerow(
+                    [
+                        eml_address,
+                        eml_reputation,
+                        eml_sus,
+                        eml_dmn_exsts,
+                        eml_dmn_rep,
+                        eml_dmn_new,
+                        eml_disposable,
+                        eml_deliverable,
+                        eml_spoofable,
+                        eml_spf_strict,
+                        eml_dmarc_enforced
+                    ]
+                )
+            except KeyError:
+                csvwriter.writerow([eml_address, None, None, None, None, None, None, None, None, None, None])
 
 
 def main():
