9e2f4e5032ad5b08a393192c7f924db61748d955
goodlandsecurity <josing.jj@gmail.com>
2022-06-27 16:05:51 -0500

    added threatfox integration

--- pyoti/multis/__init__.py
+++ pyoti/multis/__init__.py
@@ -6,6 +6,7 @@ from .misp import MISP
 from .onyphe import Onyphe
 from .otx import OTX
 from .pulsedive import Pulsedive
+from .threatfox import ThreatFox
 from .urlhaus import URLhaus
 from .urlscan import URLscan
 from .virustotal import VirusTotalV2, VirusTotalV3
