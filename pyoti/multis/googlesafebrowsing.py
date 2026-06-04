840f68b116106122ad0d3c59e396251dd851c56b
goodlandsecurity <jj.josing@rhisac.org>
2026-06-04 18:37:17 -0500

    added support for querying domains and sha256 hashes

--- pyoti/multis/googlesafebrowsing.py
+++ pyoti/multis/googlesafebrowsing.py
@@ -1,11 +1,12 @@
 import requests
+from base64 import b64encode
 from typing import Dict, List
 
 from pyoti import __version__
-from pyoti.classes import URL
+from pyoti.classes import Domain, FileHash, URL
 
 
-class GoogleSafeBrowsing(URL):
+class GoogleSafeBrowsing(Domain, FileHash, URL):
     """GoogleSafeBrowsing URL Blacklist
 
     Google Safe Browsing is a blacklist service provided by Google that
@@ -17,22 +18,42 @@ class GoogleSafeBrowsing(URL):
         api_key: str,
         api_url: str = "https://safebrowsing.googleapis.com/v4/threatMatches:find",
     ):
+        Domain.__init__(self, api_key, api_url)
+        FileHash.__init__(self, api_key, api_url)
         URL.__init__(self, api_key, api_url)
 
-    def _api_post(self, endpoint: str, platforms: List[str], **kwargs) -> requests.models.Response:
+    def _api_post(
+            self, endpoint: str, platforms: str, threat_entry: str, **kwargs
+    ) -> requests.models.Response:
         """POST request to API
 
         :param endpoint: API URL
         :param platforms: Default: ANY_PLATFORM. For all available options please see:
         https://developers.google.com/safe-browsing/v4/reference/rest/v4/PlatformType
+        :param threat_entries: digest or url. For more information please see:
+        https://developers.google.com/safe-browsing/v4/reference/rest/v4/ThreatEntry
         :return: dict of request response
         """
-        if kwargs.get('url_list'):
-            threat_entries = []
-            for url in kwargs.get('url_list'):
-                threat_entries.append({"url": url})
-        else:
-            threat_entries = [{"url": self.url}]
+        if threat_entry == 'url':
+            entry_type = 'URL'
+            if kwargs.get('url_list'):
+                entry = []
+                for url in kwargs.get('url_list'):
+                    entry.append({"url": url})
+            else:
+                entry = [{"url": self.url}]
+
+        elif threat_entry == 'digest':
+            entry_type = 'EXECUTABLE'
+            if kwargs.get('hash_list'):
+                entry = []
+                for h in kwargs.get('hash_list'):
+                    encoded_hash = b64encode(bytes.fromhex(h)).decode("utf-8")
+                    entry.append({"digest": encoded_hash})
+            else:
+                encoded_hash = b64encode(bytes.fromhex(self.file_hash)).decode("utf-8")
+                entry = [{"digest": encoded_hash}]
+
 
         data = {
             "client": {"clientId": "PyOTI", "clientVersion": f"{__version__}"},
@@ -44,9 +65,9 @@ class GoogleSafeBrowsing(URL):
                     "POTENTIALLY_HARMFUL_APPLICATION",
                     "UNWANTED_SOFTWARE",
                 ],
-                "platformTypes": platforms,
-                "threatEntryTypes": ["URL"],
-                "threatEntries": threat_entries,
+                "platformTypes": [platforms],
+                "threatEntryTypes": [entry_type],
+                "threatEntries": entry,
             },
         }
 
@@ -66,8 +87,8 @@ class GoogleSafeBrowsing(URL):
 
         return response
 
-    def check_url(self, platforms: List[str] = ["ANY_PLATFORM"]) -> Dict:
-        """Checks URL reputation
+    def check_hash(self, platforms: str = "ANY_PLATFORM") -> Dict:
+        """Checks FileHash reputation
 
         :param platforms: Default: ANY_PLATFORM. For all available options please see:
         https://developers.google.com/safe-browsing/v4/reference/rest/v4/PlatformType
@@ -75,7 +96,7 @@ class GoogleSafeBrowsing(URL):
         """
         error_code = [400, 403, 429, 500, 503, 504]
 
-        response = self._api_post(self.api_url, platforms)
+        response = self._api_post(self.api_url, platforms, "digest")
 
         if response.status_code == 200:
             if response.json() == {}:
@@ -88,17 +109,62 @@ class GoogleSafeBrowsing(URL):
             r = {'error': response.json()["error"]["message"]}
             return r
 
-    def bulk_check_url(self, url_list: List[str], platforms: List[str] = ["ANY_PLATFORM"]) -> Dict:
-        """Bulk check URL reputation
+    def bulk_check_hashes(self, hash_list: List[str], platforms: str = "ANY_PLATFORM") -> Dict:
+        """Bulk check FileHash reputation
 
-        :param url_list: List or URLs to check reputation
+        :param hash_list: List of SHA256 hashes to check reputation
         :param platforms: Default: ANY_PLATFORM. For all available options please see:
         https://developers.google.com/safe-browsing/v4/reference/rest/v4/PlatformType
         :return: dict of request response
         """
         error_code = [400, 403, 429, 500, 503, 504]
 
-        response = self._api_post(self.api_url, platforms, url_list=url_list)
+        response = self._api_post(self.api_url, platforms, "digest", hash_list=hash_list)
+
+        if response.status_code == 200:
+            if response.json() == {}:
+                r = {'matches': []}
+                return r
+            else:
+                return response.json()
+
+        elif response.status_code in error_code:
+            r = {'error': response.json()["error"]["message"]}
+            return r
+
+    def check_url(self, platforms: str = "ANY_PLATFORM") -> Dict:
+        """Checks URL reputation
+
+        :param platforms: Default: ANY_PLATFORM. For all available options please see:
+        https://developers.google.com/safe-browsing/v4/reference/rest/v4/PlatformType
+        :return: dict of request response
+        """
+        error_code = [400, 403, 429, 500, 503, 504]
+
+        response = self._api_post(self.api_url, platforms, "url")
+
+        if response.status_code == 200:
+            if response.json() == {}:
+                r = {'matches': []}
+                return r
+            else:
+                return response.json()
+
+        elif response.status_code in error_code:
+            r = {'error': response.json()["error"]["message"]}
+            return r
+
+    def bulk_check_url(self, url_list: List[str], platforms: str = "ANY_PLATFORM") -> Dict:
+        """Bulk check URL reputation
+
+        :param url_list: List of URLs to check reputation
+        :param platforms: Default: ANY_PLATFORM. For all available options please see:
+        https://developers.google.com/safe-browsing/v4/reference/rest/v4/PlatformType
+        :return: dict of request response
+        """
+        error_code = [400, 403, 429, 500, 503, 504]
+
+        response = self._api_post(self.api_url, platforms, "url", url_list=url_list)
 
         if response.status_code == 200:
             if response.json() == {}:
